5a35e18de776e906580bd766dbd76d91788af882
T.S VISHNU <146065352+VishnuThurvas@users.noreply.github.com>
2026-02-09 06:19:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
-import Debug from "./pages/Debug";
-
-<Route path="/debug" element={<Debug />} />
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
