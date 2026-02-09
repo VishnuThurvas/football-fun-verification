92684cbb09e863ed212828909a3c3d7948f4e798
T.S VISHNU <146065352+VishnuThurvas@users.noreply.github.com>
2026-02-09 06:13:23 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import Debug from "./pages/Debug";
+
+<Route path="/debug" element={<Debug />} />
+
 createRoot(document.getElementById("root")!).render(<App />);
